d1fb72e48bcec94bfd543caeb18e33c2804ee9b3
Alexashenka <sasha-sivtsov@inbox.ru>
2015-12-13 19:31:30 +0300

    again update

--- js/dbconnect.js
+++ js/dbconnect.js
@@ -4,197 +4,96 @@ $(function() {
 
     });
 
-    var nekit = false;
-    var vlad = false;
-    var dima = false;
-    var ksusha = false;
-    var flag = false;
+
+    function add_points(value, name) {
+
+
+            var routeObj = {};
+            routeObj["type"] = value;
+            $.ajax({
+                type: 'get',
+                url: 'http://ec2-52-18-236-104.eu-west-1.compute.amazonaws.com/rona/api/entertainment/',
+                data: routeObj,
+                response: 'text',
+                success: function (data) {
+                    console.log('success');
+                    for (var i = 0; i < data.results.length; i++) {
+                        var marker = new L.marker([data.results[i].latitude, data.results[i].longitude]);
+                        title = {
+                            'cost': data.results[i].cost,
+                            'ent_type': data.results[i].ent_type,
+                            'seats_count': data.results[i].seats_count,
+                            'title': data.results[i].title,
+                            'zone_title': data.results[i].zone_title
+                        };
+                        markers.addLayer(marker);
+                        map.addLayer(markers);
+                        marker.bindPopup('<b>' + 'Средняя стоимость: ' + '</b>' + data.results[i].cost + '<br>'
+                            + '<b>' + 'Тип заведения: ' + '</b>' + data.results[i].ent_type + '<br>'
+                            + '<b>' + 'Количество посадочных мест: ' + '</b>' + data.results[i].seats_count + '<br>'
+                            + '<b>' + 'Название: ' + '</b>' + data.results[i].title + '<br>'
+                            + '<b>' + 'Район: ' + '</b>' + data.results[i].zone_title);
+                        console.log(data.results[i]);
+                        console.log(markers);
+
+                    }
+                }
+            });
+            if(document.getElementById("Sonya_button").style.display == 'none') {
+                document.getElementById("Sonya_button").style.display = 'block';
+                document.getElementById(name).style.display = 'none';
+            }
+
+        }
+
+
+    function delete_points(name) {
+        map.removeLayer(markers);
+        try {
+            document.getElementById('Nekit_button').style.display = 'block';
+        }catch(e)
+        {
+        }
+        try {
+            document.getElementById('Vlad_button').style.display = 'block';
+        }catch(e)
+        {
+        }try {
+            document.getElementById('Dima_button').style.display = 'block';
+        }catch(e)
+        {
+        }try {
+            document.getElementById('Ksusha_button').style.display = 'block';
+        }catch(e)
+        {
+        }
+    }
 
     $ ('#Sonya_button').click(function() {
-        if(flag == false){
 
-            map.removeLayer(markers);
-            document.getElementById('Sonya_button').style.display = 'none';
-            flag = true;
-            }
-        else {
-            alert("На карте нет точек");
+        delete_points();
 
+    });
 
-        }
-
-    }),
     $('#Nekit_button').click(function() {
-        if (nekit == false) {
-            nekit = true;
-            flag = false;
-            var routeObj = {};
-            routeObj["type"] = 'bar';
-            $.ajax({
-                type: 'get',//тип запроса: get,post либо head
-                url: 'http://ec2-52-18-236-104.eu-west-1.compute.amazonaws.com/rona/api/entertainment/',//url адрес файла обработчика
-                data: routeObj,//параметры запроса
-                response: 'text',//тип возвращаемого ответа text либо xml
-                success: function (data) {//возвращаемый результат от сервера
-                    console.log('success');
-                    for (var i = 0; i < data.results.length; i++) {
-                        var marker = new L.marker([data.results[i].latitude, data.results[i].longitude]);
-                        title = {
-                            'cost': data.results[i].cost,
-                            'ent_type': data.results[i].ent_type,
-                            'seats_count': data.results[i].seats_count,
-                            'title': data.results[i].title,
-                            'zone_title': data.results[i].zone_title
-                        };
-                        markers.addLayer(marker);
-                        map.addLayer(markers);
-                        marker.bindPopup('<b>' + 'Средняя стоимость: ' + '</b>' + data.results[i].cost + '<br>'
-                            + '<b>' + 'Тип заведения: ' + '</b>' + data.results[i].ent_type + '<br>'
-                            + '<b>' + 'Количество посадочных мест: ' + '</b>' + data.results[i].seats_count + '<br>'
-                            + '<b>' + 'Название: ' + '</b>' + data.results[i].title + '<br>'
-                            + '<b>' + 'Район: ' + '</b>' + data.results[i].zone_title);
-                        console.log(data.results[i]);
-                        console.log(markers);
-
-                    }
-                }
-            });
-            if(document.getElementById("Sonya_button").style.display == 'none') {
-                document.getElementById("Sonya_button").style.display = 'block';
-            }
-        }
-        else{
-            alert("Бары уже добавлены на карту");
-        }
+        var bar_points = document.getElementById('Nekit_button').value;
+        document.getElementById('Nekit_button').style.display = 'none';
+        add_points(bar_points, name);
     });
-    $('#Vlad_button').click(function()
-    {   if (vlad == false) {
-        vlad = true;
-        flag = false;
-        var routeObj = {};
-        routeObj["type"] = 'cafe';
-        $.ajax({
-            type: 'get',//тип запроса: get,post либо head
-            url: 'http://ec2-52-18-236-104.eu-west-1.compute.amazonaws.com/rona/api/entertainment/',//url адрес файла обработчика
-            data: routeObj,//параметры запроса
-            response: 'text',//тип возвращаемого ответа text либо xml
-            success: function (data) {//возвращаемый результат от сервера
-                console.log('success');
-                for (var i = 0; i < data.results.length; i++) {
-                    var marker = new L.marker([data.results[i].latitude, data.results[i].longitude]);
-                    title = {
-                        'cost': data.results[i].cost,
-                        'ent_type': data.results[i].ent_type,
-                        'seats_count': data.results[i].seats_count,
-                        'title': data.results[i].title,
-                        'zone_title': data.results[i].zone_title
-                    };
-                    markers.addLayer(marker);
-                    map.addLayer(markers);
-                    marker.bindPopup('<b>' + 'Средняя стоимость: ' + '</b>' + data.results[i].cost + '<br>'
-                        + '<b>' + 'Тип заведения: ' + '</b>' + data.results[i].ent_type + '<br>'
-                        + '<b>' + 'Количество посадочных мест: ' + '</b>' + data.results[i].seats_count + '<br>'
-                        + '<b>' + 'Название: ' + '</b>' + data.results[i].title + '<br>'
-                        + '<b>' + 'Район: ' + '</b>' + data.results[i].zone_title);
-                    console.log(data.results[i]);
-                    console.log(markers);
-
-                }
-            }
-        });
-        if(document.getElementById("Sonya_button").style.display == 'none') {
-            document.getElementById("Sonya_button").style.display = 'block';
-        }
-    }else{
-        alert("Кафе уже добавлены на карту");
-    }
+    $('#Vlad_button').click(function(){
+        var bar_points = document.getElementById('Vlad_button').value;
+        document.getElementById('Vlad_button').style.display = 'none';
+        add_points(bar_points, name);
     });
-    $('#Dima_button').click(function()
-    {   if(dima == false) {
-        dima = true;
-        flag = false;
-            var routeObj = {};
-            routeObj["type"] = 'cookery';
-            $.ajax({
-                type: 'get',//тип запроса: get,post либо head
-                url: 'http://ec2-52-18-236-104.eu-west-1.compute.amazonaws.com/rona/api/entertainment/',//url адрес файла обработчика
-                data: routeObj,//параметры запроса
-                response: 'text',//тип возвращаемого ответа text либо xml
-                success: function (data) {//возвращаемый результат от сервера
-                    console.log('success');
+    $('#Dima_button').click(function(){
+        var bar_points = document.getElementById('Dima_button').value;
+        document.getElementById('Dima_button').style.display = 'none';
+        add_points(bar_points, name);
 
-                    for (var i = 0; i < data.results.length; i++) {
-                        var marker = new L.marker([data.results[i].latitude, data.results[i].longitude]);
-                        title = {
-                            'cost': data.results[i].cost,
-                            'ent_type': data.results[i].ent_type,
-                            'seats_count': data.results[i].seats_count,
-                            'title': data.results[i].title,
-                            'zone_title': data.results[i].zone_title
-                        };
-                        markers.addLayer(marker);
-                        map.addLayer(markers);
-                        marker.bindPopup('<b>' + 'Средняя стоимость: ' + '</b>' + data.results[i].cost + '<br>'
-                            + '<b>' + 'Тип заведения: ' + '</b>' + data.results[i].ent_type + '<br>'
-                            + '<b>' + 'Количество посадочных мест: ' + '</b>' + data.results[i].seats_count + '<br>'
-                            + '<b>' + 'Название: ' + '</b>' + data.results[i].title + '<br>'
-                            + '<b>' + 'Район: ' + '</b>' + data.results[i].zone_title);
-                        console.log(data.results[i]);
-                        console.log(markers);
-                    }
-                }
-            });
-        if(document.getElementById("Sonya_button").style.display == 'none') {
-            document.getElementById("Sonya_button").style.display = 'block';
-        }
-        }
-        else{
-            alert('Кулинарные магазины уже добавлены на карту');
-        }
     });
-    $('#Ksusha_button').click(function()
-    {
-        if(ksusha == false) {
-            ksusha = true;
-            flag = false;
-            var routeObj = {};
-            routeObj["type"] = 'eatery';
-            $.ajax({
-                type: 'get',//тип запроса: get,post либо head
-                url: 'http://ec2-52-18-236-104.eu-west-1.compute.amazonaws.com/rona/api/entertainment/',//url адрес файла обработчика
-                data: routeObj,//параметры запроса
-                response: 'text',//тип возвращаемого ответа text либо xml
-                success: function (data) {//возвращаемый результат от сервера
-                    console.log('success');
-                    for (var i = 0; i < data.results.length; i++) {
-                        var marker = new L.marker([data.results[i].latitude, data.results[i].longitude]);
-                        title = {
-                            'cost': data.results[i].cost,
-                            'ent_type': data.results[i].ent_type,
-                            'seats_count': data.results[i].seats_count,
-                            'title': data.results[i].title,
-                            'zone_title': data.results[i].zone_title
-                        };
-                        markers.addLayer(marker);
-                        map.addLayer(markers);
-                        marker.bindPopup('<b>' + 'Средняя стоимость: ' + '</b>' + data.results[i].cost + '<br>'
-                            + '<b>' + 'Тип заведения: ' + '</b>' + data.results[i].ent_type + '<br>'
-                            + '<b>' + 'Количество посадочных мест: ' + '</b>' + data.results[i].seats_count + '<br>'
-                            + '<b>' + 'Название: ' + '</b>' + data.results[i].title + '<br>'
-                            + '<b>' + 'Район: ' + '</b>' + data.results[i].zone_title);
-                        console.log(data.results[i]);
-                        console.log(markers);
-                    }
-
-                }
-            });
-            if(document.getElementById("Sonya_button").style.display == 'none') {
-                document.getElementById("Sonya_button").style.display = 'block';
-            }
-        }
-        else
-        {
-            alert('Закусочные уже добавлены');
-        }
+    $('#Ksusha_button').click(function(){
+        var bar_points = document.getElementById('Ksusha_button').value;
+        document.getElementById('Ksusha_button').style.display = 'none';
+        add_points(bar_points, name);
     });
 }(jQuery));
