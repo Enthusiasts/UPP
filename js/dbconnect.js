7981e719e599454f6f33b74ed99ad90b2e0e9ffd
debalid <debalid@gmail.com>
2015-12-30 19:34:55 +0300

    crunchy

--- js/dbconnect.js
+++ js/dbconnect.js
@@ -9,6 +9,14 @@ $(function() {
     //});
 
     self.initialize = function() {
+        //Global state (crunch?)
+        self.global = {
+            "cluster": {
+                "label": "checkins"
+            },
+            "ent_types": []
+        };
+
         // Initialize the map
         self.map = L.map('map').setView([55.75, 37.61], 11);
         L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -71,6 +79,71 @@ $(function() {
         routeControl.on('routingerror', function() {
             alert("hm");
         });
+
+        $ ('#Sonya_button').click(function() {
+
+            delete_points();
+
+        });
+
+        $('#Nekit_button').click(function() {
+            var bar_points = document.getElementById('Nekit_button').value;
+            document.getElementById('Nekit_button').style.display = 'none';
+            add_points(bar_points, name);
+            self.global.ent_types.push(bar_points);
+            //alert(bar_points)
+
+        });
+        $('#Vlad_button').click(function(){
+            var bar_points = document.getElementById('Vlad_button').value;
+            document.getElementById('Vlad_button').style.display = 'none';
+            add_points(bar_points, name);
+            self.global.ent_types.push(bar_points);
+        });
+        $('#Dima_button').click(function(){
+            var bar_points = document.getElementById('Dima_button').value;
+            document.getElementById('Dima_button').style.display = 'none';
+            add_points(bar_points, name);
+            self.global.ent_types.push(bar_points);
+
+        });
+        $('#Ksusha_button').click(function(){
+            var bar_points = document.getElementById('Ksusha_button').value;
+            document.getElementById('Ksusha_button').style.display = 'none';
+            add_points(bar_points, name);
+            self.global.ent_types.push(bar_points);
+        });
+
+        $('#change_cluster_checkins').click(function(){
+            self.global.cluster.label = document.getElementById('change_cluster_checkins').value;
+            //document.getElementById('change_cluster_checkins').style.display = 'none';
+
+            map.removeLayer(markers);
+            markers = createRonaMarkerClusterGroup();
+            //alert(self.global.cluster.label);
+            //alert(self.global.ent_types.length);
+            for (var i = 0; i < self.global.ent_types.length; i++)
+            {
+                add_points(self.global.ent_types[i], "");
+            }
+
+        });
+
+        $('#change_cluster_none').click(function(){
+            self.global.cluster.label = document.getElementById('change_cluster_none').value;
+            //document.getElementById('change_cluster_none').style.display = 'none';
+
+            map.removeLayer(markers);
+            markers = new L.markerClusterGroup();
+
+            //alert(self.global.cluster.label);
+            //alert(self.global.ent_types.length);
+            for (var i = 0; i < self.global.ent_types.length; i++)
+            {
+                add_points(self.global.ent_types[i], "");
+            }
+
+        });
     };
 
     self.get_marker_icon = function (num) {
@@ -107,26 +180,36 @@ $(function() {
             chunkedLoading: true,
             iconCreateFunction: function(cluster) {
                 var childs = cluster.getAllChildMarkers();
-                var childs_clusters_sum = childs
+                const childs_clusters_sum = childs
                     .map(function(marker) {
                         return marker.model.cluster_type;
                     })
                     .reduce(function(a,b) {
                         return a + b;
                     });
-                const size_type = childs.length < 100 ? 3 : 5;
-                return get_cluster_icon(Math.floor(childs_clusters_sum/childs.length), childs.length, size_type);
+
+                const childs_checkins_sum = childs
+                    .map(function(marker) {
+                        return marker.model.checkins_num;
+                    })
+                    .reduce(function(a,b) {
+                        return a + b;
+                    });
+
+
+                const size_type = childs_checkins_sum < 100 ? 3 : 5;
+                return get_cluster_icon(Math.floor(childs_clusters_sum/childs.length), childs_checkins_sum, size_type);
             }
         });
     };
 
-    function add_points(value, name) {
+    self.add_points = function(value, name) {
 
 
             var routeObj = {};
             routeObj["type"] = value;
             //routeObj["photos"] = "true";
-            routeObj["cluster"] = "checkins";
+            routeObj["cluster"] = self.global["cluster"]["label"];
             $.ajax({
                 type: 'get',
                 url: api_root + '/entertainment/',
@@ -136,10 +219,16 @@ $(function() {
                     console.log('success');
                     //console.log(data);
                     for (var i = 0; i < data.results.length; i++) {
-                        var marker = new L.marker([data.results[i].latitude, data.results[i].longitude],
+                        var marker;
+                        if (global.cluster.label == "checkins"){
+                            marker = new L.marker([data.results[i].latitude, data.results[i].longitude],
                             {
                                 icon: get_marker_icon(data.results[i].cluster_type)
                             });
+                        } else {
+                            marker = new L.marker([data.results[i].latitude, data.results[i].longitude]);
+                        }
+
                         marker.model = {
                             'instagram_urls': data.results.instagram_urls,
                             'cost': data.results[i].cost,
@@ -147,7 +236,8 @@ $(function() {
                             'seats_count': data.results[i].seats_count,
                             'title': data.results[i].title,
                             'zone_title': data.results[i].zone_title,
-                            'cluster_type': data.results[i].cluster_type
+                            'cluster_type': data.results[i].cluster_type == null ? 0 : data.results[i].cluster_type,
+                            'checkins_num': data.results[i].checkins_num
                         };
                         markers.addLayer(marker);
 
@@ -182,6 +272,7 @@ $(function() {
                                 + '<b>' + 'Тип заведения: ' + '</b>' + data.results[i].ent_type + '<br>'
                                 + '<b>' + 'Количество посадочных мест: ' + '</b>' + data.results[i].seats_count + '<br>'
                                 + '<b>' + 'Название: ' + '</b>' + data.results[i].title + '<br>'
+                                + '<b>' + 'Найдено фото:' + '</b>' + data.results[i].checkins_num +'<br/>'
                                 + '<b>' + 'Фото: ' + '</b>' + '<br>' + photo +
                                 '<br>' + '<b>' + 'Район: ' + '</b>' + data.results[i].zone_title
                                 + '<br>' + '<b>'); //+
@@ -205,6 +296,7 @@ $(function() {
 
                     }
                     map.addLayer(markers);
+                    //updateLegend();
                 }
             });
             if(document.getElementById("Sonya_button").style.display == 'none') {
@@ -212,12 +304,18 @@ $(function() {
                 document.getElementById(name).style.display = 'none';
             }
 
-        }
+        };
 
 
     function delete_points(name) {
         map.removeLayer(markers);
-        markers = createRonaMarkerClusterGroup();
+
+        if (self.global.cluster.label == "checkins") markers = createRonaMarkerClusterGroup();
+        else markers = new L.markerClusterGroup();
+
+
+        //alert(self.global.ent_types.length);
+        self.global.ent_types = [];
         try {
             document.getElementById('Nekit_button').style.display = 'block';
         }catch(e)
@@ -238,35 +336,6 @@ $(function() {
         }
     }
 
-    $ ('#Sonya_button').click(function() {
-
-        delete_points();
-
-    });
-
-    $('#Nekit_button').click(function() {
-        var bar_points = document.getElementById('Nekit_button').value;
-        document.getElementById('Nekit_button').style.display = 'none';
-        add_points(bar_points, name);
-
-    });
-    $('#Vlad_button').click(function(){
-        var bar_points = document.getElementById('Vlad_button').value;
-        document.getElementById('Vlad_button').style.display = 'none';
-        add_points(bar_points, name);
-    });
-    $('#Dima_button').click(function(){
-        var bar_points = document.getElementById('Dima_button').value;
-        document.getElementById('Dima_button').style.display = 'none';
-        add_points(bar_points, name);
-
-    });
-    $('#Ksusha_button').click(function(){
-        var bar_points = document.getElementById('Ksusha_button').value;
-        document.getElementById('Ksusha_button').style.display = 'none';
-        add_points(bar_points, name);
-    });
-
     self.createPath = function(lat_to, lon_to){
         console.log("Catched to: " + lat_to + " " + lon_to);
         var loc = map.locate()
@@ -283,4 +352,38 @@ $(function() {
     };
     //$('#map').click(onMapClick);
     //$('#map').ready(initialize());
+
+    self.updateLegend = function()
+    {
+        var params = {
+            "label": self.global.cluster.label
+        };
+
+        if (params.label==="none"){
+            document.getElementById("legend").style.display = 'none';
+        } else {
+            $.ajax({
+                type: 'get',
+                url: api_root + '/entertainment/clusterinfo/',
+                data: params,
+                //response: 'text',
+                success: function (data){
+                    //alert(data.length);
+                    for (var i = 0; i < data.length; i++) {
+                        //alert(i + " " + data.results[i].cluster_type);
+                        jQuery(
+                            "<div class='leaflet-marker-icon rona-cluster rona-cluster-3 leaflet-zoom-animated leaflet-clickable' " +
+                                "tabindex='0' " +
+                                "style='margin-left: -22px; margin-top: -30px; width: 30px; height: 30px; transform: translate3d(464px, 145px, 0px); z-index: 145;'/>" +
+                                "<img src=\"" + api_root + "/static/img/cluster/" + data.results[i].cluster_type + ".png\"/>" +
+                                "<div>"+ data.results[i].cluster_mean_rounded + "</div>" +
+                            "</div>"
+                        ).appendTo("#legend-content")
+                    }
+
+                    document.getElementById("legend").style.display = 'block';
+                }
+            });
+        }
+    }
 }(jQuery));
